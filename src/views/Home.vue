b269666bb7474b195ec034053597688c3767c960
Jake Jones <jakejonesinfo@gmail.com>
2020-11-19 01:25:07 -0500

    reformatted page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,40 @@
 <template>
   <v-container>
-	<table>
-		<tbody>
-			<tr>
-				<td>
-					<canvas id="canvas" width="900" height="700"></canvas>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-  <v-row class="d-flex align-start justify-center">
-    <v-col class="d-flex" cols="4">
-      <v-btn :loading="isLoading" color="black" dark x-large @click="printAll(tolerance)">
-        Find {{this.outputs.length}}
-      </v-btn>
-    </v-col> 
-    <v-col cols="4" class="d-flex align-center justify-center">
+  <div class="menu">
+    <v-list color="#eee">
       <v-text-field
       label="Number of colors"
       v-model="colors"
       outlined
       type="number"
-      width="20px"
       >
-
       </v-text-field>
-    </v-col>
-    <v-col cols="4" class="d-flex align-center justify-center">
       <v-text-field
       label="tolerance"
       v-model="tolerance"
       outlined
       type="number"
-      width="20px"
       >
-
       </v-text-field>
-    </v-col>
-  </v-row>
 
-  <v-row v-if="outputs && outputs.length" class="mt-8">
+      <v-text-field
+      label="link"
+      v-model="link"
+      outlined
+      type="text"
+      >
+      </v-text-field>
+
+      <v-btn :loading="isLoading" color="black" dark x-large @click="printAll(tolerance)">
+        Find
+      </v-btn>
+    </v-list>
+
+
+  </div>
+<template v-if="outputs && outputs.length">
+<h4 class="mt-8">Exacts</h4>
+  <v-row>
     <v-col cols="2" class="d-flex align-items" v-for="(item) in outputs" :key="item.value">
       <div class="prime ml-2" :style="{backgroundColor: `rgb(${item.rgb})`}">
         <div>
@@ -51,6 +46,22 @@
       </div>
     </v-col>
   </v-row>
+</template>
+
+<template v-if="clusteredAverages && clusteredAverages.length">
+  <h4 class="mt-8">Averages</h4>
+  <v-row>
+    <v-col cols="2" class="d-flex align-items" v-for="(item, index) in clusteredAverages" :key="index">
+      <div class="prime ml-2" :style="{backgroundColor: `rgb(${item.rgb})`}">
+        <div>
+        {{item.rgb}}
+        </div>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+	<canvas id="canvas" width="1500" height="1000"></canvas>
+
   </v-container>
 </template>
 
@@ -64,19 +75,32 @@ export default {
       img: new Image(),
       outputs: [],
       images: ['65535/50604677206_d4f1a369da_k.jpg', '1466/25978996034_4d049d33aa_z.jpg'],
+      link: 'https://live.staticflickr.com/65535/50604677206_d4f1a369da_k.jpg',
       colors: 5,
-      tolerance: 35,
+      tolerance: 45,
       clusteredColors: [],
-      isLoading: false
+      isLoading: false,
+      ctx: {}
+    }
+  },
+  watch: {
+    link(val){
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      this.img.src = val;
+      this.ctx = {};
+      this.ctx = this.canvas.getContext('2d');
+      this.ctx.drawImage(this.img, 0, 0);
+      this.img.style.display = 'none';
     }
   },
   mounted(){
     this.img.crossOrigin = 'anonymous';
-    this.img.src = `https://live.staticflickr.com/${this.images[1]}`;
+    this.img.src = this.link;
 
-      this.ctx = this.canvas.getContext('2d');
-      this.ctx.drawImage(this.img, 0, 0);
-      this.img.style.display = 'none';
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.drawImage(this.img, 0, 0);
+    this.img.style.display = 'none';
   },
   computed: {
     canvas(){
@@ -100,7 +124,7 @@ export default {
         var redAvg = redSum / count;
         var greenAvg = greenSum / count;
         var blueAvg = blueSum / count;
-        clusteredAverages[index] = {red: redAvg, green: greenAvg, blue: blueAvg};
+        clusteredAverages[index] = {red: redAvg, green: greenAvg, blue: blueAvg, rgb: `${Math.round(redAvg)}, ${Math.round(greenAvg)}, ${Math.round(blueAvg)}`};
       });
     return clusteredAverages;
     }
@@ -200,6 +224,22 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+#canvas {
+  transform: scale(.7);
+}
+.menu {
+  position: fixed;
+  width: 350px;
+  height: 100%;
+  background-color: #eee;
+  left: 0;
+  top: 0;
+  padding-top: 2em;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+}
     .color-cell {
       color: white;
     }
